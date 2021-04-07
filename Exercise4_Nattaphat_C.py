dfd77f9339fcb1863ea8a292620b99012dad0ae4
Meninblack420 <kokochunzz@gmail.com>
2021-04-07 17:43:45 +0700

    Create Exercise4_Nattaphat_C.py

---
+++ Exercise4_Nattaphat_C.py
@@ -0,0 +1,11 @@
+FE = 50.25     #Foundation English
+GB = 70.50     #General Business
+ICS = 80       #Introduction to Computer Systems
+CP = 100       #Computer Programming
+print("-----Your Score-----")
+print("Foundation English","                =",FE)
+print("General Business","                  =",GB)
+print("Introduction to Computer Systems","  =",ICS)
+print("Computer Programming","              =",CP)
+
+
